2a650839f1636f970f56e697725c1548ead2fe32
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-18 11:33:12 +0530

    Python Assignment 3 Deck of Cards.

---
+++ main.py
@@ -0,0 +1,7 @@
+
+# Create a deck of cards class. Internally, the deck of cards should use another class, a card class. Your requirements are:
+#
+# The Deck class should have a deal method to deal a single card from the deck
+# After a card is dealt, it is removed from the deck.
+# There should be a shuffle method which makes sure the deck of cards has all 52 cards and then rearranges them randomly.
+# The Card class should have a suit (Hearts, Diamonds, Clubs, Spades) and a value (A,2,3,4,5,6,7,8,9,10,J,Q,K)
