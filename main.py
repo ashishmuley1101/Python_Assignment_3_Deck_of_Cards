8a5a95922f1397965b464d796511713dddb31d44
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-18 21:41:44 +0530

    Python Assignment 3 : Deck of Cards distribute  to the Players and Print the Cards received by the Players.

--- main.py
+++ main.py
@@ -1,67 +1,42 @@
 
-# Create a deck of cards class. Internally, the deck of cards should use another class, a card class. Your requirements are:
-#
-# The Deck class should have a deal method to deal a single card from the deck
-# After a card is dealt, it is removed from the deck.
-# There should be a shuffle method which makes sure the deck of cards has all 52 cards and then rearranges them randomly.
-# The Card class should have a suit (Hearts, Diamonds, Clubs, Spades) and a value (A,2,3,4,5,6,7,8,9,10,J,Q,K)
+# Write a Program DeckOfCards.java, to initialize deck of cards having suit ("Clubs", "Diamonds", "Hearts", "Spades")
+# & Rank ("2", "3", "4", "5", "6", "7", "8","9", "10", "J", "Q", "K", "A"). Shuffle the cards using Random
+# method and then distribute 9 Cards to 4 Players and Print the Cards received by the 4 Players.
 
 
+import itertools
+import random
 
-from random import shuffle
+class DeckofCards:
 
-class Card:
+	def __init__(self):
+		self.suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+		self.values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
-    # Card class should have a suit (Hearts, Diamonds, Clubs, Spades) and a value (A,2,3,4,5,6,7,8,9,10,J,Q,K)
-    def __init__(self, suit, value):
-        self.suit = suit
-        self.value = value
+	def distributeCards(self,no_players,cards_distribute):
 
-    #  __repr__() function returns the object representation in string format.
-    #  This method is called when repr() function is invoked on the object
-    def __repr__(self):
-        return "(%s,%s)" % (self.value, self.suit)
-class Deck:
-    def __init__(self):
-        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-        values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+		# Creating deck of 52 cards
+		deck_cards = list(itertools.product(self.values, self.suits))
+		print("Before Shuffle : \n", deck_cards)
 
-        # creating the object of Cards class and initialized the suits and values
-        self.cards = [Card(suit, value) for suit in suits for value in values]
-        for i in self.cards:
-            print(i)
+		# Shuffling the deck of 52 cards.
+		random.shuffle(deck_cards)
+		print("After Shuffle : \n", deck_cards)
 
-    def __repr__(self):
-        return "Cards remaining in deck: {}".format(len(self.cards))
+		for i in range(no_players):
+			print("\nPlayer No.: ", i+1)
+			for j in range(cards_distribute):
+				print (deck_cards[j][0], "of", deck_cards[j][1], ",", end=" "),
+				deck_cards.pop(j)
 
-    # shuffle method which makes sure the deck of cards has all 52 cards and then rearranges them randomly.
-    def shuffle(self):
-        if len(self.cards) == 52:
-            shuffle(self.cards)   # importing the shuffle() from random module
-            return self.cards
-        else:
-            return ("Only full decks can be shuffled...!")
+# Constructor to initialize the suits and values .
+deck = DeckofCards()
 
-    # Single card is dealt, it is removed from the deck using pop()
-    def deal(self):
-        if len(self.cards) != 0:
-            return self.cards.pop()
-        else:
-            return ("All cards have been removed..!")
+# Input the values from user the numbers of players and cards have to distribute.
+no_players =int(input("Enter the numbers of Player : "))
+cards_distribute = int(input("Enter the numbers cards want to distributes : "))
+
+# Calling the distributeCards() method from the Deck class for distribution of cards.
+deck.distributeCards(no_players, cards_distribute)
 
 
-deck = Deck()
-print("Deck created...!")
-
-print("\n \nShuffling the Deck of cards..!")
-x = deck.shuffle()  # Calling the shuffle()
-for i in x:
-    print(i)
-a = int(input("\n \nEnter 1 to deal the cards : "))
-
-while (a == 1):
-    # The Deck class should have a deal method to deal a single card from the deck.
-    print(deck.deal())
-    print(deck)
-    a = int(input("Enter 1 to deal the cards (or) any key to discontinue : "))
-print(deck.shuffle())
